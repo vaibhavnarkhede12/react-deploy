41ef78ab2b26b6f228face4e98fad943bec5f943
vaibhavnarkhede12 <vaibhavnarkhede12@gmail.com>
2020-03-12 15:23:00 +0530

    Updates

---
+++ precache-manifest.ab4d1921a0b4bef7262c89f923cb71b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c383942bd81223b0fc41700bb1bc46f",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "57565425618845d28bb3",
+    "url": "/react-deploy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a4f8b0f7f248081c1b26",
+    "url": "/react-deploy/static/js/2.3e2b16b1.chunk.js"
+  },
+  {
+    "revision": "1e29a4add0501333c4ffe64ea97aa7ab",
+    "url": "/react-deploy/static/js/2.3e2b16b1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57565425618845d28bb3",
+    "url": "/react-deploy/static/js/main.eafca7e0.chunk.js"
+  },
+  {
+    "revision": "0acd0844c2597a0c8a36",
+    "url": "/react-deploy/static/js/runtime-main.570c01d6.js"
+  }
+]);
